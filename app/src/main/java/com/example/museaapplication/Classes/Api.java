ef8572b32bb8979f788e8fc80b7ef85d8466b80b
Raymaster3 <47677276+Raymaster3@users.noreply.github.com>
2021-03-21 19:03:59 +0100

    Update Api.java

--- app/src/main/java/com/example/museaapplication/Classes/Api.java
+++ app/src/main/java/com/example/museaapplication/Classes/Api.java
@@ -8,6 +8,7 @@ import com.example.museaapplication.Classes.Json.WorkValue;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
