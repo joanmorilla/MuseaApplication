6a6f256abf2287f381dd23a66267773ad2b21d05
joan.morilla <joan.morilla@estudiantat.upc.edu>
2021-04-08 17:25:05 +0200

    feature_login: added autologin

--- app/src/main/java/com/example/museaapplication/ui/signup/SignupFragment.java
+++ app/src/main/java/com/example/museaapplication/ui/signup/SignupFragment.java
@@ -1,5 +1,7 @@
 package com.example.museaapplication.ui.signup;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
