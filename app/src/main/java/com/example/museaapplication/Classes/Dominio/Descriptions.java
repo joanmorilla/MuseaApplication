f0cd763e30ced3c7448858e67116db627077cf86
Raymaster3 <47677276+Raymaster3@users.noreply.github.com>
2021-04-29 16:33:09 +0200

    Update Descriptions.java

--- app/src/main/java/com/example/museaapplication/Classes/Dominio/Descriptions.java
+++ app/src/main/java/com/example/museaapplication/Classes/Dominio/Descriptions.java
@@ -22,28 +22,4 @@ public class Descriptions implements Serializable {
                 return en;
         }
     }
-
-    public String getCa() {
-        return ca;
-    }
-
-    public void setCa(String ca) {
-        this.ca = ca;
-    }
-
-    public String getEs() {
-        return es;
-    }
-
-    public void setEs(String es) {
-        this.es = es;
-    }
-
-    public String getEn() {
-        return en;
-    }
-
-    public void setEn(String en) {
-        this.en = en;
-    }
 }
