379e1b633e51f0c3da6c64428a5e501031ee0bac
Raymaster3 <47677276+Raymaster3@users.noreply.github.com>
2021-05-18 13:18:21 +0200

    Mapa

--- app/src/main/java/com/example/museaapplication/Classes/Adapters/CustomClusterRenderer.java
+++ app/src/main/java/com/example/museaapplication/Classes/Adapters/CustomClusterRenderer.java
@@ -7,6 +7,7 @@ import com.example.museaapplication.Classes.MyClusterItem;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.Marker;
+import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
 import com.google.maps.android.clustering.view.DefaultClusterRenderer;
 
@@ -18,13 +19,28 @@ public class CustomClusterRenderer extends DefaultClusterRenderer<MyClusterItem>
 
     @Override
     protected int getColor(int clusterSize) {
-        if (clusterSize <= 10) return Color.argb(255,56, 232, 173);
-        return Color.BLUE; // Return any color you want here. You can base it on clusterSize.
+        /*if (clusterSize <= 10)*/ return Color.argb(255,51, 153, 255);
+        //return Color.BLUE; // Return any color you want here. You can base it on clusterSize.
     }
 
     @Override
     protected void onClusterItemRendered(MyClusterItem clusterItem, Marker marker) {
         marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
         super.onClusterItemRendered(clusterItem, marker);
+    }
+
+    @Override
+    protected boolean shouldRenderAsCluster(Cluster<MyClusterItem> cluster) {
+        return cluster.getSize() > 1;
+    }
+
+    @Override
+    protected String getClusterText(int bucket) {
+        return super.getClusterText(bucket).replace("+", "");
+    }
+
+    @Override
+    protected int getBucket(Cluster<MyClusterItem> cluster) {
+        return cluster.getSize();
     }
 }
